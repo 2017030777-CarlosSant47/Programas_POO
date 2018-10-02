d643eb45d6184866c72e590542d605dce60e3e79
CarlosSant47 <emci1291@gmail.com>
2018-10-01 20:32:20 -0600

    BOLETO MAIN GUI

--- src/programa1/BoletoMain.java
+++ src/programa1/BoletoMain.java
@@ -1,5 +1,8 @@
 package programa1;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 /*
                                      |####`--|#|---|##|---|#|--'##|#|
    _                                 |____,--|#|---|##|---|#|--.__|_|
@@ -11,15 +14,23 @@ import java.util.Scanner;
                               =======-===l          |_|__|_|     \##`-"__,=======.###|##|
                                                                   \__,"          '======'
 */
+
+
 public class BoletoMain {
+    private static final String PROGRAM_NAME = "Boleto";
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Object menu[] = {"1 - Crear Boleto", "2 - Calcular Subtotal", "3 - Calcular Descuento", "4 - Calcular Total a pagar",
+        "5 - Modificar Boleto", "6 - Mostrar Informacion del Boleto", "7 - Salir"};
+
+        Object tipoBoleto[] = {"1 - Simple", "2 - Redondo"};
         int numBoleto = 0, tipoViaje = 0, op = 0, edad;
         String nombre = "", destino = "", fecha = "";
         float precio = 0.0f;
-
+        String opMenu = "";
         Boleto boleto = new Boleto();
         do{
+            /*
             printf("1 - Crear Boleto");
             printf("2 - Calcular Subtotal");
             printf("3 - Calcular Descuento");
@@ -27,31 +38,51 @@ public class BoletoMain {
             printf("5 - Modificar Boleto");
             printf("6 - Mostrar Informacion del Boleto");
             printf("7 - Salir");
-            printf("Elige una opccion");
-            op = in.nextInt();
+            */
+            //printf("Elige una opccion");
+            opMenu = (String) JOptionPane.showInputDialog(null, "Elige una opccion", "Menu",
+                    JOptionPane.QUESTION_MESSAGE, null, menu, menu[0]);
+
+            for(int x = 0; x < menu.length; x++)
+            {
+                if(menu[x].toString().matches(opMenu)){
+                    op = x + 1;
+                    break;
+                }
+            }
+
             
             switch(op)
             {
                 case 1:
-                    printf("Introduzca el numero de Boleto");
-                    numBoleto = in.nextInt();
-                    printf("Intduzca el nombre");
-                    nombre = in.next();
-                    printf("Introduzca el destino");
-                    destino = in.next();
-                    printf("Introduce la fecha");
-                    fecha = in.next();
-                    printf("Introduce el precio del boleto $");
-                    precio = in.nextFloat();
+
+                    numBoleto = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduzca el numero del boleto",
+                            PROGRAM_NAME, JOptionPane.PLAIN_MESSAGE));
+                    //printf("Intduzca el nombre");
+                    nombre = JOptionPane.showInputDialog(null, "Introduzca el nombre",
+                            PROGRAM_NAME, JOptionPane.OK_OPTION);
+                    //printf("Introduzca el destino");
+                    destino = JOptionPane.showInputDialog(null, "Introduzca el destino",
+                            PROGRAM_NAME, JOptionPane.OK_OPTION);
+                    //printf("Introduce la fecha");
+                    fecha = JOptionPane.showInputDialog(null, "Introduzca el fecha",
+                            PROGRAM_NAME, JOptionPane.OK_OPTION);
+                    //printf("Introduce el precio del boleto $");
+                    precio = Float.parseFloat(JOptionPane.showInputDialog(null, "Introduzca el precio",
+                            PROGRAM_NAME, JOptionPane.OK_OPTION));
 
                     do{
-                        printf("Introduce el tipo de boleto");
-                        printf("1 - Simple");
-                        printf("2 - Redondo");
-
-                        tipoViaje = in.nextInt();
+                        String opTipo = "";
+                        opTipo = (String) JOptionPane.showInputDialog(null, "Introduce el tipo de boleto", "Menu",
+                                JOptionPane.QUESTION_MESSAGE, null, tipoBoleto, tipoBoleto[0]);
 
 
+                        for(int x = 0; x < tipoBoleto.length; x++)
+                        {
+                            if(tipoBoleto[x].toString().matches(opTipo)){
+                                tipoViaje = x + 1;
+                            }
+                        }
                     }while(tipoViaje > 2 || tipoViaje < 1);
                     boleto.setDestino(destino);
                     boleto.setFecha(fecha);
@@ -61,20 +92,28 @@ public class BoletoMain {
                     boleto.setPrecio(precio);
                     break;
                 case 2:
-
-                    printf("El subototal a pagar es $" + boleto.calcularSubtotal() + " MXN");
+                    JOptionPane.showMessageDialog(null, "El subototal a pagar es $" + boleto.calcularSubtotal() + " MXN",
+                            PROGRAM_NAME, JOptionPane.DEFAULT_OPTION);
+                    //printf("El subototal a pagar es $" + boleto.calcularSubtotal() + " MXN");
                     break;
                 case 3:
-
-                    printf("Introduce la edad");
-                    edad = in.nextInt();
-
-                    printf("El descuento por edad es de $"+ boleto.calcularDescuento(edad) + " MXN");
+                    //printf("Introduce la edad");
+                    edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduzca la edad",
+                            PROGRAM_NAME, JOptionPane.PLAIN_MESSAGE));
+                    JOptionPane.showMessageDialog(null, "El descuento por edad es de $"+ boleto.calcularDescuento(edad) + " MXN",
+                            PROGRAM_NAME, JOptionPane.DEFAULT_OPTION);
+                    //printf("El descuento por edad es de $"+ boleto.calcularDescuento(edad) + " MXN");
                     break;
                 case 4:
-                    printf("Introduce la edad");
-                    edad = in.nextInt();
-                    printf("El total a pagar es de $" + boleto.calcularTotal(edad) + " MXN");
+
+                    //printf("Introduce la edad");
+                    //edad = in.nextInt();
+                    edad = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduzca la edad",
+                            PROGRAM_NAME, JOptionPane.PLAIN_MESSAGE));
+
+                    //printf("El total a pagar es de $" + boleto.calcularTotal(edad) + " MXN");
+                    JOptionPane.showMessageDialog(null, "El total a pagar es de $" + boleto.calcularTotal(edad) + " MXN",
+                            PROGRAM_NAME, JOptionPane.DEFAULT_OPTION);
                     break;
                 case 5:
                     do{
@@ -138,8 +177,7 @@ public class BoletoMain {
                     break;
                 default: printf("Esta opccion es invalida");
             }
-            printf("Presione cualquier tecla para continuar");
-            in.next();
+
 
         }while(op != 7);
 
